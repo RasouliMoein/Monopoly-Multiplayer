90a9903e827dccc988ce0eb84c063296e9f05b35
Moein <mrgeek381@gmail.com>
2026-05-30 03:43:14 +0330

    use liara npm mirror in start.sh and stop script on failure

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 echo "Starting Monopoly Server..."
 
 # Ensure we are in the script's directory
 cd "$(dirname "$0")"
 
 echo "Installing dependencies..."
-npm install --registry="https://mirror-npm.runflare.com"
+npm install --registry="https://package-mirror.liara.ir/repository/npm/"
 
 echo "Building frontend..."
 npm run build
